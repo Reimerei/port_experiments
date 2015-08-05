36cde4e5cad64cbb210897ff47833a95ef5f8bd1
Björn Reimer <git@bjrm.de>
2015-08-05 14:36:10 +0200

    added c# code

---
+++ priv/mono/hello.cs
@@ -0,0 +1,12 @@
+using System;
+ 
+namespace hello
+{
+    class hello
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello this is csharp");
+        }
+    }
+}
